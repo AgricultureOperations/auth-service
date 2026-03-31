fd6082d04bca5b8238837cd7abe0b95e6818e6f7
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2026-03-31 16:48:28 -0500

    fix: add frontend URI to CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: "https://agricultureops.netlify.app", // your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // only needed if using cookies
   })
