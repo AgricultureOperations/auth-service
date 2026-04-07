b4ee6a531616facb69507ad93cb056d6fc3b77c8
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2026-04-07 08:54:59 -0500

    chore(config): add issuer and audience to jwt sign configuration

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,12 +1,19 @@
 import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 dotenv.config();
 
-const SECRET = process.env.JWT_SECRET || "CHANGEENV";
+const SECRET = process.env.JWT_SECRET!;
+const ISSUER = process.env.JWT_ISSUER!;
+const AUDIENCE = process.env.JWT_AUDIENCE!;
+const TIMEEXP = process.env.JWT_TIMEEXP! as SignOptions["expiresIn"];
 
 export const generateToken = (payload: object): string =>{
-    return jwt.sign(payload, SECRET, { expiresIn: "1h"})   
+    return jwt.sign(payload, SECRET, { 
+        expiresIn: TIMEEXP,
+        issuer: ISSUER,
+        audience: AUDIENCE
+    })   
 }
 
 export const verifyToken = (token: string) => {
